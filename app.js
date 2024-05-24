b4df754533109baf543ecff45928ebb4f4956645
my name <blueshaperd3@gmail.com>
2024-05-25 04:08:34 +0500

    start objects

---
+++ app.js
@@ -0,0 +1,20 @@
+var myQuestion = [
+    {
+        Question: "What is your name",
+        Answer: {
+            a: "Ali",
+            b: "Ahmed",
+            c: "Rehan"
+        },
+        CorrectAnswer: "c"
+    },
+    {
+        Question: "What is your age",
+        Answer: {
+            a: "12",
+            b: "20",
+            c: "16"
+        },
+        CorrectAnswer: "b"
+    }
+]
