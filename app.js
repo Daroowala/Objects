1848fa6bc09a5022ae27979121e17cdd4b99f5a1
my name <blueshaperd3@gmail.com>
2024-05-27 04:40:28 +0500

    not done

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
+function check(){
+    var score = 0
+var rightAnswer1 = document.getElementById("q1-a1"); 
+var qus1Answer2 = document.getElementById("q1-a2");
+var qus1Answer3 = document.getElementById("q1-a3");
+var qus1Answer4 = document.getElementById("q1-a4");
+if(rightAnswer1.checked == true){
+score++
+alert("Your Q1 Answer is Correct")
+}
+else {
+    alert("Your Q1 Answer is !!Wrong!!")
+}
 
-var myQuestion = [
-    {
-        Question: "What is your name",
-        Answer: {
-            a: "Ali",
-            b: "Ahmed",
-            c: "Rehan"
-        },
-        CorrectAnswer: "c"
-    },
-    {
-        Question: "What is your age",
-        Answer: {
-            a: "12",
-            b: "20",
-            c: "16"
-        },
-        CorrectAnswer: "b"
-    }
-]
+
+
+
+
+var rightAnswer2 = document.getElementById("q2-a1"); 
+var qus2Answer2 = document.getElementById("q2-a2");
+var qus2Answer2 = document.getElementById("q2-a3");
+var qus2Answer2 = document.getElementById("q2-a4");
+if(rightAnswer2.checked == true){
+score++
+alert("Your Q2 Answer is Correct")
+}
+else {
+    alert("Your Q2 Answer is !!Wrong!!")
+}
+}
+// var myQuestion = [
+//     {
+//         Question: "What is your name",
+//         Answer: {
+//             a: "Ali",
+//             b: "Ahmed",
+//             c: "Rehan"
+//         },
+//         CorrectAnswer: "c"
+//     },
+//     {
+//         Question: "What is your age",
+//         Answer: {
+//             a: "12",
+//             b: "20",
+//             c: "16"
+//         },
+//         CorrectAnswer: "b"
+//     }
+// ]
