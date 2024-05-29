d0e903da15fcaf063d5f5e43debda09efe358a07
my name <blueshaperd3@gmail.com>
2024-05-30 03:27:59 +0500

    Objects start

--- app.js
+++ app.js
@@ -1,66 +1,19 @@
-function check(){
-    var score = 0
-var rightAnswer1 = document.getElementById("q1-a1"); 
-var qus1Answer2 = document.getElementById("q1-a2");
-var qus1Answer3 = document.getElementById("q1-a3");
-var qus1Answer4 = document.getElementById("q1-a4");
-if(rightAnswer1.checked == true){
-score++
-alert("Your Q1 Answer is Correct")
+var plan1 = {
+    name: "Basic",
+    price: 3.99,
+    space: 100,
+    transfer: 1000,
+    pages: 10,
+    
 }
-else {
-    alert("Your Q1 Answer is !!Wrong!!")
+console.log(plan1)
+
+var webCourse = {
+    t1: "Ali",
+    t2: "Ahmed",
+    t3: "Basit",
+    
 }
-
-
-
-var rightAnswer2 = document.getElementById("q2-a1"); 
-var qus2Answer2 = document.getElementById("q2-a2");
-var qus2Answer2 = document.getElementById("q2-a3");
-var qus2Answer2 = document.getElementById("q2-a4");
-if(rightAnswer2.checked == true){
-score++
-alert("Your Q2 Answer is Correct")
-}
-else {
-    alert("Your Q2 Answer is !!Wrong!!")
-}
-
-
-
-var rightAnswer3 = document.getElementById("q3-a4"); 
-var qus3Answer1 = document.getElementById("q2-a2");
-var qus3Answer2 = document.getElementById("q2-a3");
-var qus3Answer4 = document.getElementById("q2-a4");
-if(rightAnswer3.checked == true){
-score++
-alert("Your Q3 Answer is Correct")
-}
-else {
-    alert("Your Q3 Answer is !!Wrong!!")
-}
-alert("Your score is " + score++)
-}
-
-
-
-// var myQuestion = [
-//     {
-//         Question: "What is your name",
-//         Answer: {
-//             a: "Ali",
-//             b: "Ahmed",
-//             c: "Rehan"
-//         },
-//         CorrectAnswer: "c"
-//     },
-//     {
-//         Question: "What is your age",
-//         Answer: {
-//             a: "12",
-//             b: "20",
-//             c: "16"
-//         },
-//         CorrectAnswer: "b"
-//     }
-// ]
+console.log(webCourse)
+webCourse.t4 = "Farhan"
+console.log(webCourse)
