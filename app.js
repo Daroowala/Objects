0a94b216d42089e1a653ef96b23543743f3edbc3
my name <blueshaperd3@gmail.com>
2024-08-28 23:32:32 +0500

    obj add, update and check ka tariqa

--- app.js
+++ app.js
@@ -16,4 +16,50 @@ var webCourse = {
 }
 console.log(webCourse)
 webCourse.t4 = "Farhan"
+webCourse.class = [9,12]
+
 console.log(webCourse)
+var abc = "t2" in webCourse;{
+    
+    console.log(abc)
+}
+
+var myQuestions = [
+    {
+        question: "What is your name?",
+        answer : {
+            a : "Ali",
+            b : "Rehan",
+            c : "Basit"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "What is your age?",
+        answer : {
+            a : 18,
+            b : 12,
+            c : 15
+        },
+        correctAnswer: "b"
+    },
+]
+
+// is object mai koi new obj add karna aur abj mai koi property add karna ka tarika hai aur update karna bhi hai
+let obj = {
+    FullName: "sadksa",
+    age: 18,
+    id: 5655,
+    }
+    obj.inst = "Smit"
+    obj.course = "web delvp"
+    obj.id = 222383
+    console.log(obj)
+    obj.age = 20;
+    
+    console.log(obj)
+    
+    let check = "course" in obj;
+    console.log(check)
+    
